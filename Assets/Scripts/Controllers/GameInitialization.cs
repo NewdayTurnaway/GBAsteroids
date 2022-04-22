448712c960eca603090c6b1dbf1661fceb6099a1
NewdayTurnaway <moba545@gmail.com>
2022-04-22 14:02:31 +0300

    ServiceLocator

--- Assets/Scripts/Controllers/GameInitialization.cs
+++ Assets/Scripts/Controllers/GameInitialization.cs
@@ -4,6 +4,7 @@ namespace GBAsteroids
     {
         public GameInitialization(Controllers controllers, GameData gameData)
         {
+            ServiceLocatorMono.GetService<Bullet>();
             PlayerModel playerModel = new(gameData.Player.Sprite, gameData.Player.Health, gameData.Player.Speed, gameData.Player.TurnSpeed);
             PlayerCreation playerCreation = new(playerModel);
             PlayerInitialization playerInitialization = new(playerCreation);
---
+++ Assets/Scripts/ServiceLocator/ServiceLocatorMono.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GBAsteroids
+{
+    public static class ServiceLocatorMono
+    {
+        private static readonly Dictionary<object, object> _serviceContainer;
+
+        static ServiceLocatorMono()
+        {
+            _serviceContainer = new();
+        }
+
+        public static T FindService<T>(bool createObjectNotFound = true) where T : Object
+        {
+            T type = Object.FindObjectOfType<T>();
+            
+            if (type != null)
+            {
+                _serviceContainer.Add(typeof(T), type);
+            } 
+            else if (createObjectNotFound)
+            {
+                GameObject gameObject = new(typeof(T).Name, typeof(T));
+                _serviceContainer.Add(typeof(T), gameObject.GetComponent<T>());
+            }
+
+            return (T)_serviceContainer[typeof(T)];
+        }
+
+        public static T GetService<T>(bool createObjectNotFound = true) where T : Object
+        {
+            if (!_serviceContainer.ContainsKey(typeof(T)))
+            {
+                return FindService<T>(createObjectNotFound);
+            }
+
+            T service = (T)_serviceContainer[typeof(T)];
+            if (service != null)
+            {
+                return service;
+            }
+
+            return FindService<T>(createObjectNotFound);
+        }
+    }
+}
